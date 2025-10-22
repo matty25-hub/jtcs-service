e31d948a06dec1795483d93ed64bf73fbc9c7e0e
matty25-hub <onenmathew88@gmail.com>
2025-10-22 13:57:08 +0530

    Add files via upload

---
+++ test-routes.js
@@ -0,0 +1,8 @@
+// test-routes.js
+const bookingRoutes = require('./routes/bookings');
+const contactRoutes = require('./routes/contact');
+
+console.log('Booking routes type:', typeof bookingRoutes);
+console.log('Booking routes:', bookingRoutes);
+console.log('Contact routes type:', typeof contactRoutes);
+console.log('Contact routes:', contactRoutes);
